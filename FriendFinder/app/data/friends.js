f49d19851d10b1cfd88c4ff8e0b8bf6b14dfc29e
Jiahong Rong <jiahongrong@jiahongs-mbp.attlocal.net>
2018-07-06 22:44:51 -0500

    done

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: "Stacy Rong",
+      profilePic: "https://www.facebook.com/photo.php?fbid=1330073910423197&set=a.102946586469275.3573.100002618017703&type=3&theater",
+      scores: [
+        "3",
+        "4",
+        "2",
+        "4",
+        "5",
+        "3",
+        "4",
+        "4",
+        "3",
+        "2"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
---
+++ FriendFinder/server.js
@@ -0,0 +1,23 @@
+
+//Dependencies:
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+//ROUTER
+require("./routing/apiRoutes")(app); 
+require("./routing/htmlRoutes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
